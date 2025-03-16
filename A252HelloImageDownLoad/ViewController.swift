2f8952354533a68632025169023af903524607af
Danny Shen <shenfive@gmail.com>
2025-03-16 10:05:10 +0800

    init

--- A252HelloImageDownLoad/ViewController.swift
+++ A252HelloImageDownLoad/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let url = URL(string: "https://cdn2.ettoday.net/images/5840/d5840071.jpg"){
+            
+            do{
+                let imageData = try Data(contentsOf: url)
+                theImageView.image = UIImage(data: imageData)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
+        
     }
 
 
