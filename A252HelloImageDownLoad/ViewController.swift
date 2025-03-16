90c439aad7c479501fb70c25fe4d6fa058113390
Danny Shen <shenfive@gmail.com>
2025-03-16 11:01:17 +0800

    Demo Fin

--- A252HelloImageDownLoad/ViewController.swift
+++ A252HelloImageDownLoad/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var theImageView: UIImageView!
     
+    @IBOutlet weak var theImageContaner: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         if let url = URL(string: "https://cdn2.ettoday.net/images/5840/d5840071.jpg"){
             print("start:\(Date().timeIntervalSince1970)")
+
             
             DispatchQueue.global().async {
                 do{
@@ -31,6 +33,21 @@ class ViewController: UIViewController {
         
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        theImageView.clipsToBounds = true
+        theImageView.layer.cornerRadius = theImageView.bounds.width / 2
+        theImageView.layer.borderColor = UIColor.red.cgColor
+        theImageView.layer.borderWidth = 3
+        
+        theImageContaner.clipsToBounds = false
+        theImageContaner.layer.cornerRadius = theImageContaner.bounds.width / 2
+        theImageContaner.layer.shadowRadius = 20         //陰影
+        theImageContaner.layer.shadowOpacity = 0.6;
+        theImageContaner.layer.shadowColor = UIColor.green.cgColor
+        theImageContaner.layer.shadowOffset = CGSize(width: 10, height: 10)
+
+    }
 
 }
 
