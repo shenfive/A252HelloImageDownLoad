b216bb3f87956d886c3e43a08061b5cae1e02a8e
Danny Shen <shenfive@gmail.com>
2025-03-16 10:25:46 +0800

    前景背景

--- A252HelloImageDownLoad/ViewController.swift
+++ A252HelloImageDownLoad/ViewController.swift
@@ -13,13 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         if let url = URL(string: "https://cdn2.ettoday.net/images/5840/d5840071.jpg"){
+            print("start:\(Date().timeIntervalSince1970)")
             
-            do{
-                let imageData = try Data(contentsOf: url)
-                theImageView.image = UIImage(data: imageData)
-            }catch{
-                print(error.localizedDescription)
+            DispatchQueue.global().async {
+                do{
+                    let imageData = try Data(contentsOf: url)
+                    DispatchQueue.main.async {
+                        self.theImageView.image = UIImage(data: imageData)
+                        print("downloaded:\(Date().timeIntervalSince1970)")
+                    }
+                }catch{
+                    print(error.localizedDescription)
+                }
             }
+            print("end:\(Date().timeIntervalSince1970)")
         }
         
     }
